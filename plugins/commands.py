a3209129093c9991d4278c73555690aaec9772aa
HN-MEDIA <83462108+HN-FILMCORNER@users.noreply.github.com>
2022-01-27 19:24:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -211,6 +211,8 @@ async def bot_status(client,message):
 
 @trojanz.on_message(filters.command('start') & filters.private)
 async def start(client, message):
+    if not await db.is_user_exist(update.from_user.id):
+        await db.add_user(update.from_user.id)
     await message.reply_text(
         text=Script.START_MSG.format(message.from_user.mention),
         disable_web_page_preview=True,
